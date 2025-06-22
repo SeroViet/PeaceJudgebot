1101fc6f00dd38594bced34f01acf8415580c461
SeroViet <tran.serkan@outlook.com>
2025-06-22 12:45:31 +0200

    Fix: main.py fertig gemacht

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from telegram.ext import Updater, CommandHandler
 import os
 
+# Hol dir den Bot-Token aus der Render-Umgebungsvariable
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
+# /start-Befehl
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello from PeaceJudgeBot!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="👋 Hey! Der PeaceJudgeBot ist online.")
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('start', start))
-updater.start_polling()
+if __name__ == '__main__':
+    # Starte den Bot
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Reagiere auf /start
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    print("Bot läuft ...")
+    updater.start_polling()
+    updater.idle()
+

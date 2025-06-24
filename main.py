ea0ac8e2b20c9124a6f5505f9973955af8ad72c5
SeroViet <Tran.serkan@outlook.com>
2025-06-24 22:24:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import logging
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Hey! Der PeaceJudgeBot ist online.")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-if __name__ == '__main__':
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Hey! Der PeaceJudgeBot ist online!")
+
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("✅ Bot läuft...")
     app.run_polling()
-
